17bab0a12103aa22783fcd2ae9511bb368895666
Sammit Pal <sammitpal@Sammits-Mac-mini.local>
2024-05-26 19:38:27 +0530

    added UI designs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateComponent } from './pages/create/create.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', component: HomeComponent, pathMatch: 'full'
+},{
+  path: 'createlink', component: CreateComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
